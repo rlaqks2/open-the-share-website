54aa7e1cb24ec83c383780b4e21f5ec6294bb78a
김반석 <bansuk.kim@innowireless.com>
2025-09-17 17:18:53 +0900

    build for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     base: '/open-the-share-website/', // <repository-name>으로 변경
+    build: {
+        outDir: 'docs' // 빌드 결과물을 'dist' 대신 'docs' 폴더에 저장
+    }
 })
